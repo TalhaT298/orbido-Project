edb32b683928cdeb732f6773f53c31a9e6257087
Talha <mdtalha2008@gmail.com>
2023-04-13 22:39:04 +0600

    done

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      You can capture and manage every billable event, across your entire business! Integrate your invoices directly with popular payment gateways.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
